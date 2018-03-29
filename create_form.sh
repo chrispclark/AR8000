fcb6ea73ee313e0197e47dba3a64ccc4a8164685
Chris P Clark <37695729+chrispclark@users.noreply.github.com>
2018-03-29 18:39:49 +0100

    Add files via upload

---
+++ create_form.sh
@@ -0,0 +1,4 @@
+/home/chrissy/.local/share/hatch/venvs/ar8000/bin/pyuic5 mainwindow.ui >MainWindow_ui.py
+/home/chrissy/.local/share/virtualenvs/ar8000-dISL6jZq/bin/pyuic5 mainwindow.ui >MainWindow_ui.py
+/home/chrissy/.local/share/virtualenvs/ar8000-dISL6jZq/bin/pyuic5 mainwindow.ui >MainWindow_ui.py 
+
